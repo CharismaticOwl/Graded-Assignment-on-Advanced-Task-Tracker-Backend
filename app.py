93eeae8fced9e8898256f9bd036ab3d28ebe526b
satya <narsss1234@gmail.com>
2023-10-20 23:08:33 +0530

    not much here

--- app.py
+++ app.py
@@ -52,27 +52,6 @@ def login():
         return "Login successful"
     else:
         return "Logon Failed"
-    
-
-# @app.route('/tasks', methods=['POST'])
-# def task_creation():
-
-#     return "work in progress. (Task creation page)"
-
-# @app.route('/tasks', methods=['GET'])
-# def task_retrieval():
-
-#     return "work in progress. (Task retrieval page)"
-
-# @app.route('/tasks/<task_id>', methods=['PUT'])
-# def task_update():
-
-#     return "work in progress. (Task Updation page)"
-
-# @app.route('/tasks/<task_id>', methods=['DELETE'])
-# def task_deletion():
-
-#     return "work in progress. (Task Deletion page)"
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
