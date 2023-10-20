ed48e36cfb9dfd039ee182102f9e0f37543e8860
satya <narsss1234@gmail.com>
2023-10-20 17:51:40 +0530

    Register API is created

--- app.py
+++ app.py
@@ -1,36 +1,67 @@
-from flask import Flask
+from flask import Flask, request
+import hashlib
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-@app.route('/register', methdods = ['POST'])
-def user_registration():
+client = MongoClient('mongodb://localhost:27017/')
 
-    return "work in progress. (User registration page)"
+db = client.task_tracker
 
-@app.route('/login', methods=['POST'])
-def user_login():
+@app.route('/hello', methods = ['GET'])
+def hello():
+    return "Hello World!"
 
-    return "work in progress. (User login page)"
+@app.route('/register', methods =['POST'])
+def register():
+    name = request.form.get('name')
+    email = request.form.get('email')
 
-@app.route('/tasks', methods=['POST'])
-def task_creation():
+    password = request.form.get('password')
 
-    return "work in progress. (Task creation page)"
+    password = hashlib.sha256(password.encode()).hexdigest()
 
-@app.route('/tasks', methods=['GET'])
-def task_retrieval():
+    x = db.user.find_one({
+        "name": name,
+        "email": email
+    })
 
-    return "work in progress. (Task retrieval page)"
+    if (x):
+        return "User already registered!"
+    else:
+        db.user.insert_one({
+            "name":name,
+            "email": email,
+            "password": password
+        })
 
-@app.route('/tasks/<task_id>', methods=['PUT'])
-def task_update():
+        return "User has been successfully registered"
+    
 
-    return "work in progress. (Task Updation page)"
+# @app.route('/login', methods=['POST'])
+# def user_login():
 
-@app.route('/tasks/<task_id>', methods=['DELETE'])
-def task_deletion():
+#     return "work in progress. (User login page)"
 
-    return "work in progress. (Task Deletion page)"
+# @app.route('/tasks', methods=['POST'])
+# def task_creation():
+
+#     return "work in progress. (Task creation page)"
+
+# @app.route('/tasks', methods=['GET'])
+# def task_retrieval():
+
+#     return "work in progress. (Task retrieval page)"
+
+# @app.route('/tasks/<task_id>', methods=['PUT'])
+# def task_update():
+
+#     return "work in progress. (Task Updation page)"
+
+# @app.route('/tasks/<task_id>', methods=['DELETE'])
+# def task_deletion():
+
+#     return "work in progress. (Task Deletion page)"
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
